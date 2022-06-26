5922bfa147423f5b19403dcb06ce0438a882b945
sachida nand <sachida.mv@gmail.com>
2022-06-27 01:51:18 +0530

    first commit

---
+++ app/Http/Controllers/admin/catagorie.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Catagorie;
+
+class catagorie extends Controller
+{
+
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.adminApp')
+@section('content')
+<div class="right_col" role="main">
+   <div class="card">
+       <h5>Catagory Information</h5><hr>
+       
+   </div>
+</div>
+@endsection
